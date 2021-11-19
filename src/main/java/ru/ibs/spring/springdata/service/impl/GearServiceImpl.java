9da8c4662f513b4dd958d2871867f0874be49256
Марсель <71840260+Elfenbug@users.noreply.github.com>
2021-11-20 02:14:58 +0500

    commit fix bugs

--- src/main/java/ru/ibs/spring/springdata/service/impl/GearServiceImpl.java
+++ src/main/java/ru/ibs/spring/springdata/service/impl/GearServiceImpl.java
@@ -1,4 +1,4 @@
-package ru.ibs.spring.springdata.impl;
+package ru.ibs.spring.springdata.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
